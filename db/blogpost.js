e4921e1cbf0b7b9293531019326e91f7ac8edc54
MD TAWHIDUR RAHMAN <tawhid.jony.jhm@gmail.com>
2020-02-25 23:36:29 +0600

    Update and rename blogpost.json to blogpost.js

---
+++ db/blogpost.js
@@ -0,0 +1,21 @@
+
+data:[
+  {
+    "name":"Ionic Mobile App Development",
+    "institution":"LINCOLN UNIVERSITY COLLEGE /",
+    "year":"2016 (Malaysia)",
+    "description":"I successfully completed Ionic Mobile Apps Development Workshop in 2016 from LINCOLN UNIVERSITY COLLEGE and the institution give me Certificate for my performance"
+  },
+   {
+    "name":"Ionic Mobile App Development",
+    "institution":"LINCOLN UNIVERSITY COLLEGE /",
+    "year":"2016 (Malaysia)",
+    "description":"I successfully completed Ionic Mobile Apps Development Workshop in 2016 from LINCOLN UNIVERSITY COLLEGE and the institution give me Certificate for my performance"
+  },
+   {
+    "name":"Ionic Mobile App Development",
+    "institution":"LINCOLN UNIVERSITY COLLEGE /",
+    "year":"2016 (Malaysia)",
+    "description":"I successfully completed Ionic Mobile Apps Development Workshop in 2016 from LINCOLN UNIVERSITY COLLEGE and the institution give me Certificate for my performance"
+  }
+]
